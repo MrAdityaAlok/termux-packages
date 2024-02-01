f95b52bb75ceef4d040311e4aa80a2fb62a8afb9
Aditya Alok <alok@termux.dev>
2024-02-01 16:32:02 +0530

    termux_setup_cabal: update cabal to 3.10.1.0
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- scripts/build/setup/termux_setup_cabal.sh
+++ scripts/build/setup/termux_setup_cabal.sh
@@ -1,7 +1,7 @@
 # shellcheck shell=bash
 termux_setup_cabal() {
 	if [[ "${TERMUX_ON_DEVICE_BUILD}" == "false" ]]; then
-		local TERMUX_CABAL_VERSION=3.8.1.0
+		local TERMUX_CABAL_VERSION=3.10.1.0
 		local TERMUX_CABAL_TAR="${TERMUX_COMMON_CACHEDIR}/cabal-${TERMUX_CABAL_VERSION}.tar.xz"
 
 		local TERMUX_CABAL_RUNTIME_FOLDER
@@ -16,9 +16,9 @@ termux_setup_cabal() {
 
 		[[ -d "${TERMUX_CABAL_RUNTIME_FOLDER}" ]] && return
 
-		termux_download "https://github.com/MrAdityaAlok/ghc-cross-tools/releases/download/cabal-install-v${TERMUX_CABAL_VERSION}/cabal-install-${TERMUX_CABAL_VERSION}.tar.xz" \
+		termux_download "https://github.com/MrAdityaAlok/ghc-cross-tools/releases/download/cabal-install-v$TERMUX_CABAL_VERSION/cabal-install-$TERMUX_CABAL_VERSION.tar.xz" \
 			"${TERMUX_CABAL_TAR}" \
-			6a8c3c04414cbf1c805d2c1c63a09bdc879a7babd9ac1143370bdb6f6bf5a927
+			93d5bfd3c3a00acf0711f6a0122f2079feaa6523612df22b6ba066a314ef0cdf
 
 		mkdir -p "${TERMUX_CABAL_RUNTIME_FOLDER}"
 		tar xf "${TERMUX_CABAL_TAR}" -C "${TERMUX_CABAL_RUNTIME_FOLDER}"
