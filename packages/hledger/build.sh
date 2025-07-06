afebd6f34a9b698b1ba6b0ba1faae30e0f3d7286
Aditya Alok <alok@termux.dev>
2025-07-06 16:48:20 +0530

    test:1
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- packages/hledger/build.sh
+++ packages/hledger/build.sh
@@ -6,22 +6,23 @@ TERMUX_PKG_VERSION=1.43.2
 TERMUX_PKG_SRCURL=https://github.com/simonmichael/hledger/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=60b74c70ddfc6b84ca87debd2ac302aac754da3c0d9089821182e56796cb841e
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_BUILD_DEPENDS="aosp-libs, libffi, libiconv, libgmp, zlib, ncurses"
-
-termux_step_pre_configure() {
-	termux_setup_ghc
-	termux_setup_cabal
-}
-
+TERMUX_PKG_DEPENDS="libffi, libiconv, libgmp, zlib, ncurses"
+TERMUX_PKG_BUILD_DEPENDS="aosp-libs"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-c entropy+donotgetentropy"
+#
 termux_step_post_configure() {
-	cabal get splitmix-0.1.3.1
+	cabal get splitmix-0.1.3.1 && mv splitmix{-0.1.3.1,}
+	cabal get entropy-0.4.1.11 && mv entropy{-0.4.1.11,}
 
 	for f in "$TERMUX_PKG_BUILDER_DIR"/splitmix-patches/*.patch; do
-		patch --silent -p1 -d splitmix-0.1.3.1 < "$f"
+		patch --silent -p1 -d splitmix <"$f"
 	done
 
+	sed -i -E 's|(build-type:\s*)Custom|\1Simple|' ./entropy/entropy.cabal
+
 	cat <<-EOF >>cabal.project.local
-		packages: splitmix-0.1.3.1
+		packages: splitmix
+		          entropy
 
 		package splitmix
 			benchmarks: False
